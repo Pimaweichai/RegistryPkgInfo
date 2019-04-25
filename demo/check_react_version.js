394ea5a47e9ea6fe506cf127f869df1ff0054c76
falao <falao@piduoduo.com>
2019-04-25 20:11:10 +0800

    更新

--- demo/check_react_version.js
+++ demo/check_react_version.js
@@ -1,7 +1,9 @@
 const getResitryVersion = require('../index');
 
 getResitryVersion('react').then(function(data){
-    console.log('success:'+data);
+    console.log('react 远程最新版本信息:')
+    console.dir(data);
 }).catch(function(e){
-    console.log('failed:'+e);
+    console.log('获取远程版本信息失败:')
+    console.log(e);
 })
